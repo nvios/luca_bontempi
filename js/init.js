47d30e9e8d7177e35dfb6e7b46df8501086f1a7e
Nvios <bontempiluca2@gmail.com>
2020-06-06 10:25:19 +0800

    Added cursor animation

--- js/init.js
+++ js/init.js
@@ -190,9 +190,23 @@ function openForm() {
 
 });
 
+/*----------------------------------------------------*/
+/*	cursor
+------------------------------------------------------*/
 
+const cursor = document.querySelector('.cursor');
 
+document.addEventListener('mousemove', e => {
+   cursor.setAttribute("style", "top: " + (e.pageY - 9) + "px; left: " + (e.pageX - 13) + "px;")
+})
 
+document.addEventListener('click', () => {
+   cursor.classList.add("expand");
+
+   setTimeout(() => {
+      cursor.classList.remove("expand");
+   }, 300)
+})
 
 
 
